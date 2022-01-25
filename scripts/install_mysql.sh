4cbcc8d94d20d8daa2e0d30fa1238539c3a9d7c7
Razvan Vasile <razvan.vasile@oracle.com>
2022-01-25 17:41:31 +0200

    Added Bash Scripting Tutorial 01 script

---
+++ scripts/install_mysql.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Variabile
+OS=`cat /etc/os-release | grep -w "NAME" | awk -F"=" '{print $2} | tr -d \"'`
+
+# Main
+echo "Suntem pe: $OS"
+
+if [[ $OS == "Ubuntu" ]]
+then
+  sudo apt install mysql-server
+elif [[ $OS == "CentOS Linux" ]]
+then
+  wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
+  sudo rpm -ivh mysql57-community-release-el7-9.noarch.rpm
+  sudo yum install -y mysql-server 
+else
+  echo "Nu e nici Ubuntu nici CentOS boss."
+fi
+
+
